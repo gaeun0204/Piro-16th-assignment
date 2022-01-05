9c3f5d9c4baede389cde18ccf9506cb7ceaae376
Gaeun Kim <gaeun0204@gmail.com>
2022-01-05 19:50:24 +0900

    1-4

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -2,18 +2,19 @@ class InputRangeError(Exception):
     def __init__(self):
         super().__init__('1,2,3 중 하나를 입력하세요')
 
-def InputCount():
-    while 1:
-        try:
-            cnt = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-            if (cnt < 1 or cnt >= 4):
-                raise InputRangeError
-            break
-        except ValueError:
-            print('정수를 입력하세요')
-        except InputRangeError as e:
-            print(e)
+while 1:
+    try:
+        cnt = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+        if (cnt < 1 or cnt >= 4):
+            raise InputRangeError
+        break
+    except ValueError:
+        print('정수를 입력하세요')
+    except InputRangeError as e:
+        print(e)
 
 num = 0
 
-InputCount()
+for i in range(cnt):
+    num += 1
+    print('playerA :', num)
