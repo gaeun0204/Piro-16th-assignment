f6126d66a22a95edeae7fdac082f46c6cc930fb6
Gaeun Kim <gaeun0204@gmail.com>
2022-01-05 21:37:16 +0900

    1-9

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,44 +1,49 @@
+from random import randint
+
 class InputRangeError(Exception):
     def __init__(self):
         super().__init__('1,2,3 중 하나를 입력하세요')
 
 def brGame(who):
     global num
-    global player
+    global turn
 
-    player = who # 현재 순서인 player 저장
+    turn = who
 
-    while 1:
-        try:
-            cnt = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-            if (cnt < 1 or cnt >= 4):
-                raise InputRangeError
-            break
-        except ValueError:
-            print('정수를 입력하세요')
-        except InputRangeError as e:
-            print(e)
+    if turn == 'computer':
+        cnt = randint(1, 3)
+    else:
+        while 1:
+            try:
+                cnt = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+                if (cnt < 1 or cnt >= 4):
+                    raise InputRangeError
+                break
+            except ValueError:
+                print('정수를 입력하세요')
+            except InputRangeError as e:
+                print(e)
 
     for i in range(cnt):
         num += 1
-        print('player{0} : {1}'.format(player, num))
+        print('{0} {1}'.format(turn, num))
         if num >= 31:
             break;
 
 
 global num
 num = 0
-global player
+global turn
 
 while num < 31:
-    brGame('A')
+    brGame('computer')
 
-    if num >= 31:
+    if num >= 31: # 예외처리: 종료조건
         break;
 
-    brGame('B')
+    brGame('player')
 
-if player == 'A':
-    print('playerB win!')
+if turn == 'player':
+    print('computer win!')
 else:
-    print('playerA win!')
+    print('player win!')
