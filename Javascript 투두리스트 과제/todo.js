21d67bf6a8a75fb0d76a8cecf61123cfa681a966
Gaeun Kim <gaeun0204@gmail.com>
2022-01-11 03:22:26 +0900

    .

--- Javascript 투두리스트 과제/todo.js
+++ Javascript 투두리스트 과제/todo.js
@@ -1,34 +1,34 @@
 // 항목 추가
-const addbtn = document.querySelector("#add-btn")
+const addbtn = document.querySelector("#add-btn");
 
 addbtn.addEventListener("click", function () {
-  let inputVal = document.querySelector("input")
+  let inputVal = document.querySelector("input");
 
   if (inputVal.value != "") {
-    let ul = document.querySelector("ul")
-    let li = document.createElement("li")
+    let ul = document.querySelector("ul");
+    let li = document.createElement("li");
 
-    li.innerHTML = '<input type="checkbox">' + inputVal.value
-    ul.appendChild(li)
+    li.innerHTML = '<input type="checkbox">' + inputVal.value;
+    ul.appendChild(li);
 
-    inputVal.value = "" // 입력창 비우기
+    inputVal.value = ""; // 입력창 비우기
   }
-})
+});
 
 // 전체 삭제
-const clearbtn = document.querySelector("#del-all")
+const clearbtn = document.querySelector("#del-all");
 
 clearbtn.addEventListener("click", function () {
-  let ul = document.querySelector("ul")
-  ul.innerHTML = ""
-})
+  let ul = document.querySelector("ul");
+  ul.innerHTML = "";
+});
 
 // 마지막 삭제
-const lastbtn = document.querySelector("#del-last")
+const lastbtn = document.querySelector("#del-last");
 
 lastbtn.addEventListener("click", function () {
-  let ul = document.querySelector("ul")
-  let last_li = ul.lastChild
+  let ul = document.querySelector("ul");
+  let last_li = ul.lastChild;
 
-  last_li.remove()
-})
+  last_li.remove();
+});
