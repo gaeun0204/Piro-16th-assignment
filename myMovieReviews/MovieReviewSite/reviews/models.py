5e6d819c6e843454a409caf46ad7919e587a070c
Gaeun Kim <gaeun0204@gmail.com>
2022-01-15 00:54:57 +0900

    Feat: models.py

--- myMovieReviews/MovieReviewSite/reviews/models.py
+++ myMovieReviews/MovieReviewSite/reviews/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Review(models.Model):
 	title = model.CharField(verbose_name='제목', max_length=50)
-    director = models.CharField(verbose_name='감독', max_length=20)
-    actor = models.CharField(verbose_name='주연', max_length=50)
+	director = models.CharField(verbose_name='감독', max_length=20)
+	actor = models.CharField(verbose_name='주연', max_length=50)
 	genre = models.CharField(verbose_name='장르', max_length=20)
 	rating  = models.FloatField(verbose_name='별점')
-    runningTime = models.DurationField(verbose_name='러닝타임')
-    content = models.TextField(verbose_name='리뷰내용')
+	runningTime = models.DurationField(verbose_name='러닝타임')
+	content = models.TextField(verbose_name='리뷰내용')
 
 	def __str__(self):
 		return self.title
