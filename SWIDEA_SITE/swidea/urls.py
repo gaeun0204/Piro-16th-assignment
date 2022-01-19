b6f3a0eb06ca6eedc167a00930d052548f35546e
Gaeun Kim <gaeun0204@gmail.com>
2022-01-19 23:33:15 +0900

    .

--- SWIDEA_SITE/swidea/urls.py
+++ SWIDEA_SITE/swidea/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "swidea"
 
 urlpatterns = [
-	path('', views.swidea_list, name='list'), # 앱 swidea의 urls 파일과 연결
+	path('', views.swidea_list, name = 'list'), # 앱 swidea의 urls 파일과 연결
+	path('<int:pk>/', views.swidea_detail, name = 'detail'),
+	#path('create/', view = views.swidea_create, name = 'create'),
 ]
--- SWIDEA_SITE/swidea/views.py
+++ SWIDEA_SITE/swidea/views.py
@@ -7,7 +7,7 @@ def swidea_list(request):
 	ctx = {'ideas' : ideas}
 	return render(request, template_name = 'list.html', context = ctx)
 
-# def swidea_detail(request, pk):
-# 	idea = Idea.objects.get(id=pk)
-# 	ctx = {'idea' : idea}
-# 	return render(request, template_name = 'detail.html', context = ctx)
+def swidea_detail(request, pk):
+	idea = Idea.objects.get(id=pk)
+	ctx = {'idea' : idea}
+	return render(request, template_name = 'detail.html', context = ctx)
