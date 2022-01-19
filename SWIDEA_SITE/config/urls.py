7267fefad29f7ce1319de98efffa7ecf466165f7
Gaeun Kim <gaeun0204@gmail.com>
2022-01-20 00:30:50 +0900

    ,

--- SWIDEA_SITE/config/urls.py
+++ SWIDEA_SITE/config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('', include('swidea.urls')), # 앱 swidea의 urls 파일과 연결
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
